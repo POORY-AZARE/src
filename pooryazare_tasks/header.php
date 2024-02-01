03cc5fcc8d149da3e9e1fbbd51fbe9b355ef8101
POORY-AZARE <142793636+POORY-AZARE@users.noreply.github.com>
2024-02-01 18:00:06 +0200

    Add files via upload

---
+++ pooryazare_tasks/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Web Programming</title>
+    <link rel="stylesheet" href="stylesheet.css" />
+  </head>
+  <body>
+    <header>
+      <nav class="navbar">
+        <div class="logo">Web Programming</div>
+       
+        <div class="nav-links">
+         <a href="ex1.php">EXERCISE 1</a>
+          <a href="ex2.php">EXERCISE 2</a>
+          <a href="ex3.php">EXERCISE 3</a>
+          <a href="ex4.php">EXERCISE 4</a>
+          <a href="ex5.php">EXERCISE 5</a>
+          
+        </div>
+        <div class="language-switcher">
+          <a href="?lang=en">EN</a> /
+          <a href="?lang=fi">FI</a>
+        </div>
+      </nav>
+      <!-- New Section for Special Message -->
+      <div class="special-message">
+
+      </div>
+
+      
+      <div class="search-bar">
+        
+      </div>
+
+      <div class="search-bar">
+        
+      </div>
+    </header>
+
+    <main>
