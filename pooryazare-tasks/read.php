ec314d8e3b4c09307fbcb2d93c0af64007612bcc
POORYAZARE <pooryazare.2019@gmail.com>
2024-02-15 21:07:03 +0200

    creat sections

--- pooryazare-tasks/read.php
+++ pooryazare-tasks/read.php
@@ -1,10 +1,10 @@
 <?php include 'header.php'; ?>
-<?php
 
+<?php
 include 'pz.php'; 
 
-// SQL query to retrieve data from the 'TestDriveBookings' table
-$sql = "SELECT * FROM TestDriveBookings";
+// SQL query to retrieve data from the 'reservations' table
+$sql = "SELECT * FROM reservations";
 
 // Execute the SQL query and store the result
 $result = $conn->query($sql);
@@ -14,27 +14,31 @@ if ($result->num_rows > 0) {
     echo "<table class='table'>
             <thead>
                 <tr>
-                    <th>BookingID</th>
+                    <th>ID</th>
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Email</th>
                     <th>Phone Number</th>
-                    <th>Vehicle Model</th>
-                    <th>Test Drive Date</th>
+                    <th>Reservation Date</th>
+                    <th>Reservation Time</th>
+                    <th>Guests Number</th>
+                    <th>Special Requests</th>
                 </tr>
             </thead>
             <tbody>";
 
-    // Loop through the result set and display data in rows
+    // Loop through the result set and display data in table rows
     while ($row = $result->fetch_assoc()) {
         echo "<tr>
-                <td><a href='updatesingle.php?id=$row[BookingID]'>$row[BookingID]</a></td>
-                <td>{$row['FirstName']}</td>
-                <td>{$row['LastName']}</td>
-                <td>{$row['Email']}</td>
-                <td>{$row['PhoneNumber']}</td>
-                <td>{$row['VehicleModel']}</td>
-                <td>{$row['Date']}</td>
+                <td>{$row['id']}</td>
+                <td>{$row['first_name']}</td>
+                <td>{$row['last_name']}</td>
+                <td>{$row['email']}</td>
+                <td>{$row['phone_number']}</td>
+                <td>{$row['reservation_date']}</td>
+                <td>{$row['reservation_time']}</td>
+                <td>{$row['guests_number']}</td>
+                <td>{$row['special_requests']}</td>
               </tr>";
     }
 
@@ -47,4 +51,5 @@ if ($result->num_rows > 0) {
 // Close the database connection when done
 $conn->close();
 ?>
+
 <?php include 'footer.php'; ?>
